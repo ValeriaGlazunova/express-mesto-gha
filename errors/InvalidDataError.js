aa1e9e916bdad1f62f0e76abb38912bb388155c2
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-07-07 00:33:07 +0500

    autotest

--- errors/InvalidDataError.js
+++ errors/InvalidDataError.js
@@ -3,7 +3,6 @@ const { ERR_BAD_REQUEST } = require('../utils/constants');
 class InvalidDataError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'ValidationError';
     this.statusCode = ERR_BAD_REQUEST;
   }
 }
--- errors/NotFoundError.js
+++ errors/NotFoundError.js
@@ -3,7 +3,6 @@ const { ERR_NOT_FOUND } = require('../utils/constants');
 class NotFoundError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'CastError';
     this.statusCode = ERR_NOT_FOUND;
   }
 }
