078954a00ddfc0867df174b379bb12abf6841f92
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-07-13 21:26:53 +0500

    Добавить валидацию ссылки карточки

--- models/card.js
+++ models/card.js
@@ -11,10 +11,8 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
-    validate: {
-      validator(v) {
-        return linkRegExp.test(v);
-      },
+    validator(v) {
+      return linkRegExp.test(v);
     },
   },
   owner: {
