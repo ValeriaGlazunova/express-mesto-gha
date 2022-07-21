60d1024f9d9ba10945aa7b8f84af2831863a77a0
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-07-21 16:54:47 +0500

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,6 +5,7 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
+// eslint-disable-next-line consistent-return
 module.exports = ((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
@@ -13,13 +14,13 @@ module.exports = ((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.status(200).send();
-    return;
+    return res.status(200).end();
   }
 
   next();
